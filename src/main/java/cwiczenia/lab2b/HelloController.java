6820e031626de761da42e371f14b4c2a16756dd4
JDrewniak2 <saylen223@wp.pl>
2022-06-14 19:19:49 +0200

    Dodanie grafiki

--- src/main/java/cwiczenia/lab2b/HelloController.java
+++ src/main/java/cwiczenia/lab2b/HelloController.java
@@ -5,13 +5,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 
 public class HelloController {
+    public Button btnPokazInfografike;
+    public Button btnPrzejdzDoStrony;
+    public ImageView imgMiniatura;
+    public TextField txtAdresStrony;
     @FXML
     private Label lbFile;
     FileChooser fileChooser = new FileChooser();
