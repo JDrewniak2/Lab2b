620ef48b5a6843f2a81f9faf169f46a49ed3d6c2
JDrewniak2 <saylen223@wp.pl>
2022-06-14 19:34:45 +0200

    dodanie public void changed

--- src/main/java/cwiczenia/lab2b/HelloController.java
+++ src/main/java/cwiczenia/lab2b/HelloController.java
@@ -1,6 +1,8 @@
 package cwiczenia.lab2b;
 
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
@@ -30,6 +33,23 @@ public class HelloController {
     @FXML
     public void initialize(){
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val) {
+                        int index = new_val.intValue();
+                        if (index != -1){
+                            txtAdresStrony.setText(igList.infografiki.get(index).adresStrony);
+                            Image image = new Image(igList.infografiki.get(index).adresGrafiki);
+                            imgMiniatura.setImage(image);
+                        }
+                        else{
+                            txtAdresStrony.setText("");
+                            imgMiniatura.setImage(null);
+                        }
+                    }
+                }
+        );
     }
     public void btnOpenFileAction(ActionEvent actionEvent) {
         File file = fileChooser.showOpenDialog(null);
